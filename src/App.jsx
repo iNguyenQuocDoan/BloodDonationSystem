519b642ea9a21ca3c08ff5e77ff2c8ad55210984
iNguyenQuocDoan <quocdoanfb@gmail.com>
2025-06-20 07:34:49 +0700

    Add admin layout

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import AboutUs from "./pages/home/AboutUs";
 import "./App.css";
 import LayoutDeFault from "./Layouts/LayoutDefault";
 import Homepage from "./pages/home";
-import EmergencyRequest from "./pages/home/EmergencyRequest";  // đã thêm trang EmergencyRequest 6/6/2025 By Vượng
+import EmergencyRequest from "./pages/home/EmergencyRequest"; // đã thêm trang EmergencyRequest 6/6/2025 By Vượng
 import DonateBlood from "./pages/home/DonateBlood";
 import { FAQPage } from "./pages/home/FAQ";
 import LayoutStaff from "./Layouts/LayoutStaff";
@@ -20,6 +20,9 @@ import DashboardPage from "./pages/Staff/Dashboard";
 import EditBloodPage from "./pages/Staff/EditBlood";
 import ManageEmergencyPage from "./pages/Staff/ManageEmergency";
 import ConfirmBloodPage from "./pages/Staff/ConfirmBlood";
+import AdminLayout from "./Layouts/LayoutAdmin";
+import CreateSlot from "./pages/Admin/CreateSlot";
+import ManageEmergencyRequest from "./pages/Admin/EmergencyRequest";
 
 function App() {
   return (
@@ -30,14 +33,14 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/forgot-password" element={<ForgotPasswordPage />} />
             <Route path="/register" element={<RegisterPage />} />
-            <Route path="/contact" element={<Contact />} />   {/* đã sửa thêm trang contact nha*/}
-            <Route path="aboutus" element={<AboutUs />} /> {/* đã sửa thêm trang about us 6/6/2025 đã đông bộ với footer*/}
+            <Route path="/contact" element={<Contact />} />{" "}
+            {/* đã sửa thêm trang contact nha*/}
+            <Route path="aboutus" element={<AboutUs />} />{" "}
+            {/* đã sửa thêm trang about us 6/6/2025 đã đông bộ với footer*/}
             <Route path="/emergency" element={<EmergencyRequest />} />
             <Route path="/contact" element={<Contact />} />{" "}
             {/* đã sửa thêm trang contact nha*/}
             <Route path="/" element={<Homepage />} />
-
-
             <Route path="/contact" element={<Contact />} />
             <Route path="faq" element={<FAQPage />} />
             <Route path="donate" element={<DonateBlood />} />
@@ -47,8 +50,16 @@ function App() {
             <Route path="create-slot" element={<CreateSlotPage />} />
             <Route path="dashboard" element={<DashboardPage />} />
             <Route path="confirm-blood" element={<ConfirmBloodPage />} />
-            <Route path="edit-blood" element={<EditBloodPage />}/>
+            <Route path="edit-blood" element={<EditBloodPage />} />
             <Route path="manage-emergency" element={<ManageEmergencyPage />} />
+          </Route>
+
+          <Route element={<AdminLayout />}>
+            <Route path="create-slot" element={<CreateSlot />} />
+            <Route
+              path="emergency-request"
+              element={<ManageEmergencyRequest />}
+            />
           </Route>
         </Routes>
       </BrowserRouter>
